4839a8d3a23d45556a1532646c4207272ed4908b
91077063 <31664291+91084699@users.noreply.github.com>
2017-09-18 10:48:26 -0700

    added houses

--- src/org/teachingkidsprogramming/section02methods/HousesB.java
+++ src/org/teachingkidsprogramming/section02methods/HousesB.java
@@ -1,9 +1,9 @@
-package org.teachingkidsprogramming.section02methods.Variations;
+package org.teachingkidsprogramming.section02methods;
 
 import org.teachingextensions.logo.Tortoise;
-import org.teachingextensions.logo.utils.ColorUtils.PenColors;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors.Reds;
 
-public class Houses2
+public class HousesB
 {
   public static void main(String[] args)
   {
@@ -12,15 +12,25 @@ public class Houses2
     Tortoise.setX(200);
     int height = 40;
     drawHouse(height);
+    drawHouse(30);
     drawHouse(120);
+    drawHouse(150);
     drawHouse(90);
+    drawHouse(70);
     drawHouse(20);
+    drawHouse(100);
+    drawHouse(200);
+    //drawHousesSlantedRoof
   }
+  //define a method called drawHouse SlantedRoof
+  //define a method called DrawSlantedRoof
   private static void drawHouse(int height)
   {
-    Tortoise.setPenColor(PenColors.Reds.Red);
+    Tortoise.setPenColor(Reds.Red);
     Tortoise.move(height);
-    drawFlatRoof();
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(90);
     Tortoise.move(height);
     Tortoise.turn(-90);
     Tortoise.move(20);
