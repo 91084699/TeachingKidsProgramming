3592ecd60c0326ef0e11087acf560fbee5e3166b
91077063 <31664291+91084699@users.noreply.github.com>
2017-09-22 12:54:17 -0700

    worked on challenges

--- src/org/teachingkidsprogramming/section02methods/PyramidsOfGiza.java
+++ src/org/teachingkidsprogramming/section02methods/PyramidsOfGiza.java
@@ -8,22 +8,8 @@ public class PyramidsOfGiza
   public static void main(String[] args) throws Exception
   {
     setUpPyramidLand();
-    //  Turn the tortoise 90 degrees to the left 
-    Tortoise.turn(-90);
-    //  Move the tortoise 220 pixels
-    Tortoise.move(220);
-    //  Turn the tortoise 135 degrees to the right 
-    Tortoise.turn(135);
     //  Move the tortoise 100 pixels
-    Tortoise.move(100);
-    //  Turn the tortoise 90 degrees to the right
-    Tortoise.turn(90);
-    //  Move the tortoise 100 pixels
-    Tortoise.move(100);
-    //  Turn the tortoise 90 degrees to the left 
-    Tortoise.turn(-90);
-    //  Move the tortoise 100 pixels
-    Tortoise.move(100);
+    /* Tortoise.move(100);
     //  Turn the tortoise 90 degrees to the right 
     Tortoise.turn(90);
     //  Move the tortoise 100 pixels
@@ -40,9 +26,24 @@ public class PyramidsOfGiza
     Tortoise.turn(135);
     //  Move the tortoise 210 pixels
     Tortoise.move(210);
-  }
+    */
+  } // end of main
   private static void setUpPyramidLand()
   {
+    //  Turn the tortoise 90 degrees to the left 
+    Tortoise.turn(-90);
+    //  Move the tortoise 220 pixels
+    Tortoise.move(220);
+    //  Turn the tortoise 135 degrees to the right 
+    Tortoise.turn(135);
+    //  Move the tortoise 100 pixels
+    Tortoise.move(100);
+    //  Turn the tortoise 90 degrees to the right
+    Tortoise.turn(90);
+    //  Move the tortoise 100 pixels
+    Tortoise.move(100);
+    //  Turn the tortoise 90 degrees to the left 
+    Tortoise.turn(-90);
     Tortoise.show();
     Tortoise.setSpeed(10);
     Tortoise.getBackgroundWindow().setBackground(PenColors.Blues.AliceBlue);
