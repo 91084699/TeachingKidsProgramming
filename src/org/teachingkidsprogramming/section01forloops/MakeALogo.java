83203054233fb001620e068fb217989bfd896c3d
91077063 <31664291+91084699@users.noreply.github.com>
2017-11-28 10:00:15 -0800

    worked on adlibs

--- src/org/teachingkidsprogramming/section01forloops/MakeALogo.java
+++ src/org/teachingkidsprogramming/section01forloops/MakeALogo.java
@@ -1,5 +1,7 @@
 package org.teachingkidsprogramming.section01forloops;
 
+import org.teachingextensions.logo.Tortoise;
+
 public class MakeALogo
 {
   public static int YValue = 300;
@@ -7,7 +9,9 @@ public class MakeALogo
   public static void main(String[] args)
   {
     //  Show the Tortoise --#1
+    Tortoise.show();
     //  Make the Tortoise draw a line as fast as possible --#2
+    Tortoise.setSpeed(10);
     //  Setup the Color Wheel --#3 HINT: Use the TKPLogo object
     //  Draw a TKP 'T' --#4
     //  Draw a left bracket --#8
